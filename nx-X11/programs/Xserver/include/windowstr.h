6e6f2a0b711ba7652cdf00c6c1c1a39f7ef33e61
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2016-04-19 11:02:14 +0200

    XEVIE: Drop all Xserver code blocks that relate to removed XEVIE extension.

--- nx-X11/programs/Xserver/include/windowstr.h
+++ nx-X11/programs/Xserver/include/windowstr.h
@@ -227,8 +227,4 @@ extern ScreenSaverStuffRec savedScreenInfo[MAXSCREENS];
 extern int numSaveUndersViewable;
 extern int deltaSaveUndersViewable;
 
-#ifdef XEVIE
-extern WindowPtr xeviewin;
-#endif
-
 #endif /* WINDOWSTRUCT_H */
